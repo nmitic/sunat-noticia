3df41c410c2bb5209e9edbab9015f2c3fb3f05d6
Nikola Mitic <nikola.mitic.dev@gmail.com>
2026-02-04 12:56:05 -0500

    allowed to be embeded in iframe by local host and app.perunio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' http://localhost:5173 https://app.perunio.pe",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
