9518c9c143a63f33f913f55801dd2aa8c48ad700
Nikola Mitic <nikola.mitic.dev@gmail.com>
2026-02-04 10:40:09 -0500

    logo fix

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo({ className = "" }: { className?: string }) {
     return (
         <Image
-            src="/logo/FullLogo_Transparent_NoBuffer.png"
+            src="/logo/fulllogo_transparent_nobuffer.png"
             alt="Perunio Logo"
             width={180}
             height={60}
